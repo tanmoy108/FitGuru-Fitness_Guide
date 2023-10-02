bcaa2e5a47229cda70d1da32b0e30c1009f570a2
shtanmoy <tanmoysharma46@gmail.com>
2023-10-02 21:55:34 +0600

    api update

--- src/components/exerciseField/ExerciseForm.jsx
+++ src/components/exerciseField/ExerciseForm.jsx
@@ -24,7 +24,7 @@ const ExerciseForm = () => {
             alignItems="center"
             spacing={3}
             my={5}>
-            <h2 style={{textAlign:"center"}} >Awesome Exercises You
+            <h2 style={{textAlign:"center",marginTop:"50px"}} >Awesome Exercises You
                 Should Know</h2>
             <Stack direction={{md:"row",xs:"column"}} >
             <TextField id="outlined-basic" label="Search Exercises" variant="outlined" sx={{width:{lg:'500px'}}} value={search} onChange={HandleChange} />
--- src/utils/utilsFetchData.js
+++ src/utils/utilsFetchData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const exerciseOption = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_KEY,
+        'X-RapidAPI-Key': 'def5a06f22msh0385ea801e530cdp14f836jsn393e7fe7d702',
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
 };
